f1d54828b06ec63942517e9c87cfcc320d9933fe
Marcel <Marcel.Weller@novatec-gmbh.de>
2019-05-08 09:44:48 +0200

    added minikube script and moved kubernetes files

---
+++ kubernetes/minikube/create.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+MK_NAMESPACE=todo-application
+
+# Make sure to use local Minikube installation
+kubectl config use-context minikube	
+
+# Create the namespace
+kubectl create ns ${MK_NAMESPACE}
+
+# Create the configmaps from files
+kubectl create configmap postgres-exporter-conf \
+--namespace=${MK_NAMESPACE} \
+--from-file=../../config/queries.yml 
+
+kubectl create configmap init-db-conf \
+--namespace=${MK_NAMESPACE} \
+--from-file=../../config/initdb.sh 
+
+# Create the application resources 
+kubectl apply -f ./kubernetes/
